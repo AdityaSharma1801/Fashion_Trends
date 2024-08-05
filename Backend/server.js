fd007ad207b1ab96e88fc6b6bcb1c5b46c40f817
aditya sharma <sharmaaditya18012004@gmail.com>
2024-08-05 11:25:36 +0530

    server essential files and folders

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,18 @@
-const express =  require("express")
-const colors = require("colors")
-const dotenv = require("dotenv")
-const morgan = require("morgan")
+const express =  require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+const rootroute = require ("./routes/rootroute")
+
+const PORT = process.env.PORT || 6000;
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("api is working");
+});
+
+app.use('/fashiontrends',rootroute);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`.bgBlue.white)
+})
